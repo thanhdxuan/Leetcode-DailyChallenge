35dcd1cac38646dde8069fbedaa4892d34d26fda
thanhdxuan <hello.thanhdauxuan@gmail.com>
2023-03-24 00:29:41 +0700

    update: depth first search

--- March2023/22-minScore.h
+++ March2023/22-minScore.h
@@ -35,3 +35,31 @@ public:
         return bfs(n, adj);
     }
 };
+class Other: public Solution {
+  public:
+    void dfs(int& n, vector<vector<pair<int, int>>>& adjs, vector<bool>& visited, int& ans) {
+        visited[n] = true;
+        auto adj_arr = adjs[n];
+        for (auto adj : adj_arr) {
+            if (!visited[adj.first]) {
+                cout << adj.first << " " << adj.second << " " << ans << endl;
+                dfs(adj.first, adjs, visited, ans);
+            }
+            ans = min(ans, adj.second);
+        }
+    }
+
+    int minScore(int n, vector<vector<int>>& roads) {
+        vector<vector<pair<int, int>>> adj(n + 1);
+        vector<bool> visited(n, false);
+        for (auto& road : roads) {
+            adj[road[0]].push_back({road[1], road[2]});
+            adj[road[1]].push_back({road[0], road[2]});
+            //(neighbor, weight)
+        }
+        int ans = INT_MAX;
+        int k = 1;
+        dfs(k, adj, visited, ans);
+        return ans;
+    }
+};
